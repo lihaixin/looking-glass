71ea8cb7b741208429fa020090266198678484b7
lihaixin <lihaixin@15099.net>
2022-10-07 22:31:43 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH;ntp &
+
+if [ -f "/etc/envfile" ]; then
+export $(grep -v '^#' /etc/envfile | xargs)
+fi
+
+
+cd LookingGlass
+chmod +x autoconfig.sh
+chown nginx: ./*
+chown nginx: ratelimit.db
+
+nginx
+php-fpm7 --nodaemonize --fpm-config /etc/php7/php-fpm.conf -c /etc/php7/php.ini
