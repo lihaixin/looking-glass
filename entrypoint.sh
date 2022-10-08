6b859f4ed0afe1694f0b8b32a6c09e3bc36a67d9
lihaixin <lihaixin@15099.net>
2022-10-08 14:17:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,17 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH;ntp &
 
 if [ -f "/etc/envfile" ]; then
-export $(grep -v '^#' /etc/envfile | xargs)
+export $(grep -v '^#' /etc/envfile | grep -v LG_testfiles | xargs)
 fi
+
+IP=$(curl -s  ip.sb)
+COUNTRY=`curl ipinfo.io/country  2>/dev/null || curl ipinfo.io/country 2>/dev/null`
+
+: ${LG_ip4:=q$IP}
+: ${LG_loc:=q$COUNTRY}
+: ${LG_sitename:=Looking Glass}
+: ${LG_testfiles:=25MB 50MB 100MB}
+: ${LG_siteurl:=https://lg.my.site}
 
 set -e
 cd /app/LookingGlass
